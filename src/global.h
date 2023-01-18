9ba0cb6dc71b25c472dd7d3641c910f4827e97e1
nim <nim@debian>
2023-01-18 09:51:13 -0500

    [start]

--- src/global.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "graphics.h"
-#include <stdbool.h>
-
-extern struct graphics *g_graphics;
-extern SDL_Window *window;
-extern SDL_Renderer *renderer;
-extern size_t const g_plots_count;
-extern SDL_Point mouse;
