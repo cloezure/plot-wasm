dedd111a075fe3c8a5e92529487d8886207090af
nim <nim@debian>
2023-01-18 09:50:30 -0500

    [bug] off_channel not work

---
+++ .ccls-cache/@home@nim@dev@c@plot-wasm/src@colorscheme.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+
+#define COLOR_WHITE                                                            \
+  (SDL_Color) { 0xFF, 0xFF, 0xFF, 0xFF }
+
+#define COLOR_GREEN                                                            \
+  (SDL_Color) { 0x00, 0xFF, 0x00, 0xFF }
+
+#define COLOR_BACKGROUND                                                       \
+  (SDL_Color) { 0x26, 0x26, 0x26, 0xFF }
+
+#define COLOR_PLOT_NAME COLOR_WHITE
+
+#define COLOR_CHANNEL_NUMBER_ON                                                \
+  (SDL_Color) { 0x38, 0x38, 0x38, 0xFF }
+
+#define COLOR_CHANNEL_NUMBER_OFF                                               \
+  (SDL_Color) { 0x2C, 0x2C, 0x2C, 0xFF }
+
+#define COLOR_CHANNEL_HEAD                                                     \
+  (SDL_Color) { 0x76, 0x76, 0x76, 0xFF }
+
+#define COLOR_PLOT_LINE                                                        \
+  (SDL_Color) { 0x3B, 0x94, 0xE5, 0xFF }
+
---
+++ .ccls-cache/@home@nim@dev@c@plot-wasm/src@text.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <stdint.h>
+
+struct text {
+  SDL_Rect position;
+  SDL_Texture *texture;
+  TTF_Font *font;
+  char *content;
+  SDL_Color color;
+};
+
+enum TEXT_FONT_TYPE { TEXT_FONT_REGULAR = 0, TEXT_FONT_BOLD = 1 };
+char const *text_get_font_type(enum TEXT_FONT_TYPE type);
+
+struct text *text_cons(char const *font_path, int32_t font_size,
+                       SDL_Color color, SDL_Point position,
+                       char const *content);
+
+void text_free(struct text *text);
+void text_change_content(struct text *text, char const *content);
+void text_change_color(struct text *text, SDL_Color color);
